483064ac98f3feeee70d28f267480f791ba47a8c
Kassim Ouattara <ouat.kassim@gmail.com>
2017-11-30 16:08:34 -0500

    Connecting reducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addReminder } from "../actions";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ""
+        }
+    }
+    addReminder(){
+        this.props.addReminder(this.state.text);
+    }
     render(){
         return(
             <div className="App">
@@ -9,13 +21,17 @@ class App extends Component {
                 </div>
                 <div className="form-inline">
                     <div className="form-group">
-                        <input className="form-control" placeholder="I have to..."/>
+                        <input className="form-control" placeholder="I have to..." onChange={event => this.setState({text: event.target.value})}/>
                     </div>
-                    <button type="button" className="btn btn-success">Add reminder</button>
+                    <button type="button" className="btn btn-success" onClick={() => this.addReminder()}>Add reminder</button>
                 </div>
             </div>
         )
     }
 }
 
-export default App;
+function mapDispacthToProps(dispatch){
+    return bindActionCreators({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispacthToProps)(App);
